940471fa2c5403f4e41cb27ad79c70eae828ed00
imrabti <imrabti@gmail.com>
2012-10-06 12:29:18 +0000

    Fixed code format.

--- src/main/java/com/nuvola/gxpenses/server/util/SecurityUtils.java
+++ src/main/java/com/nuvola/gxpenses/server/util/SecurityUtils.java
@@ -4,9 +4,9 @@ import org.springframework.security.authentication.encoding.PasswordEncoder;
 import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 
 public class SecurityUtils {
-	
-	public static String encodePasswordSha1(String password, String salt) {
-		PasswordEncoder encoder = new ShaPasswordEncoder();
-		return encoder.encodePassword(password, salt);
-	}
+
+    public static String encodePasswordSha1(String password, String salt) {
+        PasswordEncoder encoder = new ShaPasswordEncoder();
+        return encoder.encodePassword(password, salt);
+    }
 }
