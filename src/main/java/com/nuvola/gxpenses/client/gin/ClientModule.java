e1322e2d3b3cbea9c155be08134be56e3b8759bc
imrabti <imrabti@gmail.com>
2012-11-06 11:07:31 +0000

    Added cloudfoundry configuration.

--- src/main/java/com/nuvola/gxpenses/client/gin/ClientModule.java
+++ src/main/java/com/nuvola/gxpenses/client/gin/ClientModule.java
@@ -54,6 +54,7 @@ public class ClientModule extends AbstractPresenterModule {
         bind(Integer.class).annotatedWith(PageSize.class).toProvider(PageSizeProvider.class);
         bindConstant().annotatedWith(DefaultPlace.class).to(NameTokens.login);
         bindConstant().annotatedWith(Names.named("rest")).to("http://127.0.0.1:8888/rest");
+        //bindConstant().annotatedWith(Names.named("rest")).to("http://gxpensesapp.cloudfoundry.com/rest");
     }
 
     @Provides
